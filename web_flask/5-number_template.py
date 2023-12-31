7cb508fa80b1c9618f8fe94b907e4bdf3ddf3d34
ondanje <cyrilondanje@gmail.com>
2023-12-31 13:44:39 +0300

    rendering templates

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """
+    display “C ” followed by the value of the text
+    variable (replace underscore _ symbols with a space )
+    """
+    return "C {}".format(text.replace('_', ''))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is cool'):
+    return "Python {}".format(text.replace('_', ''))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_page(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    """Run the Flask app on 0.0.0.0, port 5000"""
+    app.run(host='0.0.0.0', port=5000)
