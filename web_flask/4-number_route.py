ff0bd0e5434abeb8e63ff6b534bfe1056d9c53b4
ondanje <cyrilondanje@gmail.com>
2023-12-31 13:15:16 +0300

    display “n is a number” only if n is an integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -25,7 +25,6 @@ def display_c(text):
     return "C {}".format(text.replace('_', ''))
 
 
-@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text='is cool'):
     return "Python {}".format(text.replace('_', ''))
