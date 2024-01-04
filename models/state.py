83bf933fd499b46988c853190c3ba96c2e384ea8
ondanje <cyrilondanje@gmail.com>
2024-01-04 09:24:34 +0300

    updated state model

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -18,7 +19,7 @@ class State(BaseModel, Base):
         """getter cities attributes from file storage"""
         from models import storage
         city_list = []
-        for city in storage.all(city).values():
+        for city in storage.all(City).values():
             if city.state_id == self.id:
                 city_list.append(city)
         return city_list
