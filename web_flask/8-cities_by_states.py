c58dbe7c00a11c680e0957a79c899b0cf3130cb3
ondanje <cyrilondanje@gmail.com>
2024-01-04 14:48:24 +0300

    task 9 correction

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,12 +9,6 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states():
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def display_cities_by_states():
     states = storage.all(State)
