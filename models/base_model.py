edeceb2b56bce516a9751884d86701e7aa277536
ondanje <cyrilondanje@gmail.com>
2024-01-04 09:23:58 +0300

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -15,20 +15,39 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
+    # def __init__(self, *args, **kwargs):
+    #   """Instatntiates a new model"""
+    #   if not kwargs:
+    #       from models import storage
+    #       self.id = str(uuid.uuid4())
+    #       self.created_at = datetime.now()
+    #       self.updated_at = datetime.now()
+    #   else:
+    #       kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+    #                                                '%Y-%m-%dT%H:%M:%S.%f')
+    #       kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+    #                                                '%Y-%m-%dT%H:%M:%S.%f')
+    #       del kwargs['__class__']
+    #       self.__dict__.update(kwargs)
+    #
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        """
+        Instatntiates a new model
+        """
+        # from models import storage
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ("created_at", "updated_at"):
+                    self.__dict__[k] = datetime.\
+                                       strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "__class__":
+                    continue
+                else:
+                    self.__dict__[k] = v
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
